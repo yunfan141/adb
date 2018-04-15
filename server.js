a0883fe193e577559659fd023f0ac4e31181ee0f
yunfan zhou <yunfanzhou@yunfans-MacBook-Pro.local>
2018-04-14 20:52:24 -0400

    first demo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser= require('body-parser')
 const assert = require('assert');
 const app = express()
 const url = 'mongodb://localhost:27017';
-const dbName = 'terr';
+const dbName = 'users';
 
 const MongoClient = require('mongodb').MongoClient
 
@@ -14,93 +14,435 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+let tabledata = [];
+let country = [];
+let year = [];
+let motive = [];
+let attack = [];
+let weapon = [];
+let nation = [];
+let query1 = [];
+let query2 = [];
+let query3 = [];
+let query4 = [];
+let query5 = [];
+let query6 = [];
+let query7 = [];
+let query8 = [];
+let query9 = [];
+let query10 = [];
+let query11 = [];
+
 MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
   
     db = client.db(dbName);
-    app.listen(3000, () => {
-        console.log('listening on 3000')
-      })
+    db.collection('Terrorism').aggregate([
+        {
+           $group:
+             {
+               _id: "$iyear",
+             }
+        },
+        {
+            $sort:{ _id : 1}
+        }]).toArray((err, result) => {
+        if (err) return console.log(err)
+        year = result;
+    });
+    
+    db.collection('Terrorism').aggregate([
+        {
+            $group:
+                {
+                _id: "$country_txt",
+                }
+        },
+        {
+            $sort:{ _id : 1}
+        }]).toArray((err, result) => {
+        if (err) return console.log(err)
+        country = result;
+    });
+
+    db.collection('Terrorism').aggregate([
+        {
+            $group:
+                {
+                _id: "$attacktype1_txt",
+                }
+        },
+        {
+            $sort:{ _id : 1}
+        }]).toArray((err, result) => {
+        if (err) return console.log(err)
+        attack = result;
+        console.log(attack);
+    });
+
+    db.collection('Terrorism').aggregate([
+        {
+            $group:
+                {
+                _id: "$weaptype1_txt",
+                }
+        },
+        {
+            $sort:{ _id : 1}
+        }]).toArray((err, result) => {
+        if (err) return console.log(err)
+        weapon = result;
+        console.log(weapon);
+    });
+
+    db.collection('Terrorism').aggregate([
+        {
+           $group:
+             {
+               _id: "$natlty1_txt",
+             }
+        },
+        {
+            $sort:{ _id : -1}
+        },
+     ]).toArray((err, result) => {
+        if (err) return console.log(err)
+        nation = result;
+        console.log(nation);
+     });
+    // db.collection('Terrorism').aggregate([
+    //     { 
+    //         $match: {nperpcap : {$gt: 10} }
+    //     },
+    //     {
+    //        $group:
+    //          {
+    //            _id: "$motive",
+    //            num_perpetrators_captured: { $sum: "$nperpcap" }
+    //          }
+    //     }
+    //  ]).toArray((err, result) => {
+    //     if (err) return console.log(err)
+    //     motive = result;
+    //     console.log(motive);
+    // });
+    
   });
 
-// app.get('/', (req, res) => {
-//       res.render('MongoDB.ejs')
-//   })
+// for the table displayed
 app.get('/', (req, res) => {
-    db.collection('terrorism').aggregate([
-        {
-           $group:
-             {
-               _id: "$country_txt",
-               num_terrorist_attacks: { $sum: 1 }
-             }
-        },
-        {
-            $sort:{ num_terrorist_attacks : -1}
-        },
-        {
-            $limit : 1
-        }
-     ]).toArray((err, result) => {
-        if (err) return console.log(err)
-        res.render('index.ejs', {quotes: result})
-      })
-  })
-
-  app.get('/a', (req, res) => {
-    res.render('MongoDB.ejs')
-  })
-
-  app.post('/quotes', (req, res) => {
-    var country = req.body.name;
-    db.collection('terrorism').aggregate([
-        {
-           $match: {suicide : 1}
+    db.collection('Terrorism').aggregate([
+        { 
+            $match: {nperpcap : {$gt: 0} }
         },
         {
            $group:
              {
-               _id: "$country_txt",
-               num_suicide_attack: { $sum: 1 }
+               _id: "total_num_perpetrators_captured",
+               total_num: { $sum: "$nperpcap" },
+               count: { $sum: 1 }
              }
-        },
-        {
-            $sort:{ num_suicide_attack : -1}
-        },
-        {
-            $match: {_id : req.body.name}
         }
+    
      ]).toArray((err, result) => {
         if (err) return console.log(err)
-        res.render('index.ejs', {quotes: result})
-        console.log(result)
-      })
+        tabledata = result;
+        res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+        console.log(tabledata);
+        console.log(year[1]);
+      },
+    )
   })
 
-  app.post('/quotes2', (req, res) => {
-    var country = req.body.name;
-    db.collection('terrorism').aggregate([
-        {
-           $match: {suicide : 1}
+// form post for query 1
+  app.post('/query1', (req, res) => {
+      let yearpicked = parseInt(req.body.yearpicker);
+
+    db.collection('Terrorism').aggregate([
+        { 
+            $match: {nperpcap : {$gt: 0} }
         },
         {
            $group:
              {
-               _id: "$country_txt",
-               num_suicide_attack: { $sum: 1 }
-             }
-        },
+               _id: "$iyear",
+               total_num: { $sum: "$nperpcap" },
+             }},
         {
-            $sort:{ num_suicide_attack : -1}
-        },
-        {
-            $match: {_id : req.body.name}
+            $match: {_id : yearpicked}
         }
      ]).toArray((err, result) => {
         if (err) return console.log(err)
-        res.render('index.ejs', {quotes: result})
-        console.log(result)
+        query1 = result;
+        res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+        console.log(query1)
+        console.log(yearpicked);
       })
   })
 
+//form post for query 2
+  app.post('/query2', (req, res) => {
+    let countrypicked = req.body.countrypicker;
+
+  db.collection('Terrorism').aggregate([
+	{
+       $group:
+         {
+           _id: "$country_txt",
+           num_terrorist_attacks: { $sum: 1 }
+         }
+    },
+    {
+        $sort:{ num_terrorist_attacks : -1}
+    },
+    {$match: {_id: countrypicked}}
+    ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query2 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query2)
+      console.log(countrypicked);
+    })
+})
+
+//form post for query 3 ***** test using Italy ******
+app.post('/query3', (req, res) => {
+    let countrypicked = req.body.countrypicker;
+
+  db.collection('Terrorism').aggregate([
+    { 
+		$match: {ransompaid : {$gt: -1} }
+	},
+	{
+       $group:
+         {
+           _id: "$country_txt",
+           num_ransom_paid: { $sum: "$ransompaid" }
+         }
+    },
+    { 
+		$match: { _id: countrypicked }
+	}
+
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query3 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query3)
+      console.log(countrypicked);
+    })
+})
+
+//form post for query 4 **** test using Yemen *****
+app.post('/query4', (req, res) => {
+    let countrypicked = req.body.countrypicker;
+
+  db.collection('Terrorism').aggregate([
+    { 
+		$match: {ransomamt : {$gt: -1} }
+	},
+	{
+       $group:
+         {
+           _id: "$country_txt",
+           num_ransom_demanded: { $sum: "$ransomamt" }
+         }
+    },
+    { 
+		$match: { _id: countrypicked }
+	}
+
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query4 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query4)
+      console.log(countrypicked);
+    })
+})
+
+//form post for query 5 ****  *****
+app.post('/query5', (req, res) => {
+    let attackpicked = req.body.attackpicker;
+
+  db.collection('Terrorism').aggregate([
+    { 
+      $match: {success : "Yes"}
+  },
+  {
+     $group:
+       {
+         _id: "$attacktype1_txt",
+         num_wound: { $sum: "$nwound" }
+       }
+  },
+    { 
+		$match: { _id: attackpicked }
+	}
+
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query5 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query5)
+      console.log(attackpicked);
+    })
+})
+
+//form post for query 6 ****test using Irap *****
+app.post('/query6', (req, res) => {
+    let countrypicked = req.body.countrypicker;
+
+  db.collection('Terrorism').aggregate([
+    {
+       $match: {suicide : "Yes"}
+    },
+	{
+       $group:
+         {
+           _id: "$country_txt",
+           num_suicide_attack: { $sum: 1 }
+         }
+    },
+    { 
+		$match: { _id: countrypicked }
+	}
+
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query6 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query6)
+      console.log(countrypicked);
+    })
+})
+
+//form post for query 7 ****just show the table *****
+app.post('/query7', (req, res) => {
+    let motivepicked = req.body.motivepicker;
+
+  db.collection('Terrorism').aggregate([
+    { 
+        $match: {nperpcap : {$gt: 10} }
+    },
+    {
+       $group:
+         {
+           _id: "$motive",
+           num_perpetrators_captured: { $sum: "$nperpcap" }
+         }
+    },
+    {
+        $limit: 10
+    }
+
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query7 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query7);
+      console.log(motivepicked);
+    })
+})
+
+app.post('/query8', (req, res) => {
+    let typepicked= req.body.typepicker;
+
+  db.collection('Terrorism').aggregate([
+	{
+       $group:
+         {
+           _id: "$attacktype1_txt",
+           num_attack: { $sum: 1 }
+         }
+    },
+    { 
+		$match: { _id: typepicked }
+	}
+
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query8 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query8)
+      console.log(typepicked);
+    })
+})
+
+
+app.post('/query9', (req, res) => {
+    let wtypepicked= req.body.wtypepicker;
+
+  db.collection('Terrorism').aggregate([
+	{
+       $group:
+         {
+           _id: "$weaptype1_txt",
+           num_attack: { $sum: 1 }
+         }
+    },
+    { 
+		$match: { _id: wtypepicked }
+	}
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query9 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query9)
+      console.log(wtypepicked);
+    })
+})
+
+app.post('/query10', (req, res) => {
+    let nationpicked= req.body.nationpicker;
+
+  db.collection('Terrorism').aggregate([
+	{
+        $group:
+          {
+            _id: "$natlty1_txt",
+            num_terrorist_attacks: { $sum: 1 }
+          }
+     },
+    { 
+		$match: { _id: nationpicked }
+	}
+ ]).toArray((err, result) => {
+      if (err) return console.log(err)
+      query10 = result;
+      res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+      console.log(query10)
+      console.log(nationpicked);
+    })
+})
+
+app.post('/query11', (req, res) => {
+  let countrypicked= req.body.countrypicker;
+
+db.collection('Terrorism').aggregate([
+  {
+    $group:
+      {
+        _id: {country: "$country_txt", weapontype: "$weaptype1_txt"},
+      }
+ },
+ {
+    $match: { "_id.country" : countrypicked}
+ }
+]).toArray((err, result) => {
+    if (err) return console.log(err)
+    query11 = result;
+    res.render('mongodb.ejs', {tabledata,year,attack,country,motive,weapon,nation,query1,query2,query3,query4,query5,query6,query7,query8,query9,query10,query11})
+    console.log(query11)
+    console.log(countrypicked);
+  })
+})
+
+    
+
+
+var server = app.listen(3000, function() {
+  var port = server.address().port;
+  console.log('Express server listening on port %s.', port);
+})
